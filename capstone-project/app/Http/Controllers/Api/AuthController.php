0a4185b8044be4e710ae98353e8722e60ea92920
akash <akash.ju.msc@gmail.com>
2026-06-30 14:47:52 +0600

    week-5: add register api in day 7

--- capstone-project/app/Http/Controllers/Api/AuthController.php
+++ capstone-project/app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
 use App\Services\AuthService;
 
 class AuthController extends Controller
@@ -14,9 +15,10 @@ class AuthController extends Controller
 
     public function register(RegisterRequest $request)
     {
-        $this->authService->register(
+        $user = $this->authService->register(
             $request->validated()
         );
 
+        return new UserResource($user);
     }
 }
